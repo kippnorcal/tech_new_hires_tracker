4f49563e15a07435f276f49d0bfbaee68e989c84
Mark Young <markyoung.ca@gmail.com>
2023-04-06 10:22:05 -0700

    Adjusted timedelta in eval_sla_met func

--- jobs/sla_monitor.py
+++ jobs/sla_monitor.py
@@ -49,7 +49,7 @@ def compare_dates_new_col(df, new_col: str, date_col1: str, date_col2: str) -> N
 def eval_sla_met(df) -> None:
     df["TechCleared_MetSLA_Boolean"] = np.where(pd.isnull(df["DateCleared"]),
                                                 np.where(date.today() >= df["StartDate"].dt.date, 0, 1),
-                                                np.where((df["DateCleared"] + timedelta(days=3)) <= df["StartDate"], 1, 0))
+                                                np.where((df["DateCleared"] + timedelta(days=1)) <= df["StartDate"], 1, 0))
 
 
 def create_sla_denominator_field(df):
