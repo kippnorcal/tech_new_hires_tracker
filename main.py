3533fb31b23f1d212c73c22b4ecbc1d3a9c426f5
Mark Young <markyoung.ca@gmail.com>
2023-10-23 17:21:02 -0700

    Set up job-notifications package

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
 import traceback
 
+from job_notifications import create_notifications
 from pygsheets import authorize, Spreadsheet
 
 from jobs.sla_monitor import refresh_sla_source
 from jobs.refresh_tracker import tracker_refresh
 from utils.arg_parser import create_parser
 from utils.logger_config import get_logger
-from utils.mailer import Mailer
 
 TECH_TRACKER_SHEET = os.getenv("TECH_TRACKER_SHEETS_ID")
 HR_MOT_SHEET = os.getenv("HR_MOT_SHEETS_ID")
@@ -25,24 +25,25 @@ def create_sheet_connection(sheet_key: str) -> Spreadsheet:
     return client.open_by_key(sheet_key)
 
 
-def main(mailer):
+def main():
     tech_spreadsheet = create_sheet_connection(TECH_TRACKER_SHEET)
     if ARGS.sla_monitor_refresh:
-        mailer.job_name_extend('SLA Monitor Refresh')
         refresh_sla_source(tech_spreadsheet)
     else:
         school_year = ARGS.school_year[0]
-        mailer.job_name_extend(school_year)
         hr_mot_spreadsheet = create_sheet_connection(HR_MOT_SHEET)
         tracker_refresh(tech_spreadsheet, hr_mot_spreadsheet, school_year)
 
 
 if __name__ == "__main__":
-    mailer = Mailer(f"Tech On-boarding Tracker")
+    if ARGS.sla_monitor_refresh:
+        notifications = create_notifications("Tech On-boarding Tracker - SLA Monitor Refresh",
+                                             "mailgun", logs="app.log")
+    else:
+        notifications = create_notifications(f"Tech On-boarding Tracker - {ARGS.school_year[0]}",
+                                             "mailgun", logs="app.log")
     try:
-        main(mailer)
-        mailer.notify()
+        notifications.notify()
     except Exception as e:
         stack_trace = traceback.format_exc()
-        logger.error(stack_trace)
-        mailer.notify(success=False)
+        notifications.notify(error_message=stack_trace)
