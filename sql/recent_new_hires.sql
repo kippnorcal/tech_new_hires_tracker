77562ee6a29e5de112b21ed05e1a24f837f0312f
Mark Young <markyoung.ca@gmail.com>
2023-11-02 13:55:41 -0700

    Removed unnecessary fields from query

--- sql/recent_new_hires.sql
+++ sql/recent_new_hires.sql
@@ -1,19 +1,15 @@
 SELECT DISTINCT
     c.job_eid + '-' +c.candidate_eid AS job_candidate_id
-    ,c.firstName AS first_name
-    ,c.lastName AS last_name
-    ,c.is_this_a_rehire_promotion_or_internal_transfer as hire_reason
-    ,c.email AS personal_email
-    ,c.assigned_work_location AS work_location
-    ,c.assigned_pay_location AS pay_location
-    ,c.startDate AS start_date
-    ,c.title AS title
-    ,c.formerOrCurrentKIPP AS "former_or_current_kipp"
-    --SpEd? column will be generated here by Python
-    , c.disposition
-    ,c.lastUpdatedDate AS last_updated_date
+    ,c.firstName AS "First Name"
+    ,c.lastName AS "Last Name"
+    ,c.is_this_a_rehire_promotion_or_internal_transfer as "New, Returners, Rehire or Transfer"
+    ,c.email AS "Personal Email"
+    ,c.assigned_work_location AS "Work Location"
+    ,c.assigned_pay_location AS "Pay Location"
+    ,c.startDate AS "Start Date"
+    ,c.title AS "Title"
+    ,c.formerOrCurrentKIPP AS "Former or Current KIPP"
 FROM custom.jobvite_full c
 WHERE lastUpdatedDate >= DATEADD(MONTH, -1, GETDATE())
     AND lastUpdatedDate < GETDATE()
     AND workflowState = 'Offer Accepted'
-ORDER BY lastUpdatedDate DESC
